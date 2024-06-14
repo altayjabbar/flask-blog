046d87bbd628bd8147c6ea9100a683015b80b16a
altayjabbar <jabbarofaltay@gmail.com>
2024-06-14 22:24:58 +0400

    flask--form

--- app.py
+++ app.py
@@ -38,9 +38,15 @@ def register():
     
     return render_template("register.html",title='Register',form = form)
 
-@app.route('/login')  
+@app.route('/login',methods=["GET","POST"])  
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@block.com' and form.password.data == 'password':
+            flash("You have been logged in!",'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Log in Unsuccessful.Please check username and password','danger')
     return render_template("login.html",title='Login',form = form)
 
 
