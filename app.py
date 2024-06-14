39920eec1c70f7ac8ae7866be5d9b8c3ef5c73b2
altayjabbar <jabbarofaltay@gmail.com>
2024-06-14 17:52:04 +0400

    added flask-form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,flash,redirect
+from flask import Flask, render_template, url_for,flash,redirect
 from forms import registrationForm,LoginForm
 app = Flask(__name__)
 
