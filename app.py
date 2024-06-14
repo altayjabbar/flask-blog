be53cde0f4a8f63c35ae67c4dbc67f28c364e3db
altayjabbar <jabbarofaltay@gmail.com>
2024-06-14 10:30:26 +0400

    flask-bootstrap

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 from flask import Flask, render_template
+# from forms import registrationForm,LoginForm
 app = Flask(__name__)
+
+# app.confing['SECRET_KEY'] = '6ddc470b93ea6b43e950a0ad94121b27b17391a513b1c8d4'
 
 posts = [
     {
-      'name':"jeo felix",
-      'work':"coding",
-      'start_date':"2022,nowember 18"
+      'author':"jeo felix",
+      'title':"coding",
+      'date_posted':"2022,nowember 18"
       
     },
     {
-        'name':"albert",
-      'work':"coding",
-      'start_date':"2022,nowember 10"
-      
+        'author':"jeo felix",
+      'title':"coding",
+      'date_posted':"2022,nowember 18"
+    
     }
 ]
 
 @app.route('/')
-def hello_name():
-    return render_template("index.html",posts=posts)
+@app.route('/home')
+def home():
+    return render_template("home.html",posts=posts)
 
 @app.route('/about')
 def about():
-    return "<h1>about page</h1>"
+    return render_template("about.html",title='About')
+
+# @app.route('/registr')  
+# def register():
+#     form = registrationForm()
+#     return render_template(register.html,)
 
 if __name__ =="__main__":
     app.run(debug = True)
