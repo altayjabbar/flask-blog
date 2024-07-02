55b92a73e1e2784db764dbc0a395d238098ef03d
altayjabbar <jabbarofaltay@gmail.com>
2024-07-02 16:19:33 +0400

    User  Account and Profile Picture

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6ddc470b93ea6b43e950a0ad94121b27b17391a513b1c8d4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 with app.app_context():
     db.create_all()
 bcrypt = Bcrypt(app)
@@ -14,4 +17,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-from flaskblog import routes
+from flaskblog import routes, models # noqa
