bc5f3246f6909918c3f0f6fab88231a556bbed3a
altayjabbar <jabbarofaltay@gmail.com>
2024-07-04 20:32:11 +0400

    added: reset password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -16,5 +18,10 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True 
+app.config['MAIL_USERNAME'] = "shukranjsp@gmail.com"
+app.config['MAIL_PASSWORD'] = 'hmkw zipe bidz dyrz'
+mail = Mail(app)
 from flaskblog import routes, models # noqa
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,28 +1,51 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default2.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default="default2.jpg")
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship("Post", backref="author", lazy=True)
 
-    def __repr__(self):
+    def get_reset_token(self, expires_sec=1800):
+        serializer = URLSafeTimedSerializer(app.config["SECRET_KEY"])
+        return serializer.dumps(self.id, salt="reset-password")
+
+    @staticmethod
+    def verify_reset_token(token):
+        serializer = URLSafeTimedSerializer(
+            app.config["SECRET_KEY"], salt="reset-password"
+        )
+        try:
+            user_id = serializer.loads(token, max_age=1800)
+        except SignatureExpired:
+            # Handle expired token (optional)
+            return None
+        except BadSignature:
+            # Handle invalid token
+            return None
+        return User.query.get(user_id)
+
+    def _repr_(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
-    def __repr__(self):
+    def _repr_(self):
         return f"Post('{self.title}', '{self.date_posted}')"
