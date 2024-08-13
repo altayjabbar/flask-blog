6cb473c8e178d55ceca8570bdfa0493329593010
altayjabbar <jabbarofaltay@gmail.com>
2024-08-13 17:09:14 +0400

    admin panel

--- flaskblog/admin/posts/routes.py
+++ flaskblog/admin/posts/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from flaskblog.models import Post
 from flask_login import current_user
 from flaskblog.common.utils import admin_required, save_picture
+from flaskblog import db
 
 posts = Blueprint("posts", __name__, url_prefix="/posts")
 
@@ -10,37 +11,48 @@ posts = Blueprint("posts", __name__, url_prefix="/posts")
 @admin_required
 def list_or_create_posts():
     if request.method == "POST":
-        image = request.files.get("image")
-        title = request.form["title"]
-        short_desc = request.form["short_desc"]
-        content = request.form["content"]
-        new_post = Post(
-            title=title, content=content, short_desc=short_desc, author=current_user
-        )
-        if image:
-            picture_file = save_picture(image, "posts/media")
-            new_post.image_file = picture_file
-        new_post.save_to_db()
-        return redirect(url_for("posts.list_or_create_posts"))
+        try:
+            image = request.files.get("image")
+            title = request.form["title"]
+            short_desc = request.form["short_desc"]
+            content = request.form["content"]
+            new_post = Post(
+                title=title, content=content, short_desc=short_desc, author=current_user
+            )
+            if image:
+                picture_file = save_picture(image, "posts/media")
+                new_post.image_file = picture_file
+            db.session.add(new_post)
+            db.session.commit()
+            return jsonify({"message": "Post created successfully"}), 201
+        except Exception as e:
+            return jsonify({'message':str(e)}),500
+        
     posts_list = Post.query.all()
-    return render_template("admin/posts.html", posts=posts_list)
+    return render_template('admin/posts.html',posts = posts_list)
 
 
-@posts.route("/update/<int:post_id>", methods=["GET", "PUT"])
+@posts.route("/update/<int:post_id>", methods=["PUT"])
 @admin_required
 def update_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    if request.method == "PUT":
+    try:
+        post = Post.query.get_or_404(post_id)
         post.title = request.form["title"]
+        post.short_desc = request.form["short_desc"]
         post.content = request.form["content"]
-        post.update_db()
-        return redirect(url_for("posts.list_or_create_posts"))
-    return render_template("admin/update_post.html", post=post)
+        db.session.commit()
+        return jsonify({"message": "Post updated successfully"}), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
 
 @posts.route("/delete/<int:post_id>", methods=["DELETE"])
 @admin_required
 def delete_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    post.delete_from_db()
-    return redirect(url_for("posts.list_or_create_posts"))
+    try:
+        post = Post.query.get_or_404(post_id)
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({"message": "Post deleted successfully"}), 204
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
--- flaskblog/admin/users/routes.py
+++ flaskblog/admin/users/routes.py
@@ -1,47 +1,58 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_bcrypt import Bcrypt
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
 from flaskblog.common.utils import admin_required, handle_hashed_password_generate
 from flaskblog.models import User
 from flask_login import login_user, logout_user, current_user
 from flaskblog.users.forms import LoginForm
+from flaskblog import db
 
 users = Blueprint("users", __name__, url_prefix="/users")
 
-bcrypt = Bcrypt()
+
 
 
 @users.route("/", methods=["GET", "POST"])
 @admin_required
 def list_or_create_users():
     if request.method == "POST":
-        name = request.form["username"]
-        email = request.form["email"]
-        password = handle_hashed_password_generate(request.form["password"])
-        new_user = User(username=name, email=email, password=password)
-        new_user.save_to_db()
-        return redirect(url_for("users.list_or_create_users"))
+        try:
+            name = request.form["username"]
+            email = request.form["email"]
+            password = handle_hashed_password_generate(request.form["password"])
+            new_user = User(username=name, email=email, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return jsonify({"message": "Users created successfully"}), 201
+        except Exception as e:
+                    return jsonify({'message':str(e)}),500
+
     users_list = User.query.all()
-    return render_template("admin/users.html", users=users_list)
+    return render_template('admin/users.html', users= users_list)
 
 
-@users.route("/update/<int:user_id>", methods=["GET", "POST"])
+@users.route("/update/<int:user_id>", methods=["PUT"])
 @admin_required
 def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-    if request.method == "POST":
+    try:
+        user = User.query.get_or_404(user_id)
         user.username = request.form["username"]
         user.email = request.form["email"]
-        user.update_db()
-        return redirect(url_for("users.list_or_create_users"))
-    return render_template("admin/update_user.html", user=user)
+        db.session.commit(user)
+        return jsonify({"message": "User updated successfully"}), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
 
-@users.route("/delete/<int:user_id>", methods=["POST"])
+
+@users.route("/delete/<int:user_id>", methods=["DELETE"])
 @admin_required
 def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    user.delete_from_db()
-    return redirect(url_for("users.list_or_create_users"))
+    try:
+        user = User.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()        
+        return jsonify({"message": "User was deleted successfully"}), 204
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
 
 @users.route("/login", methods=["GET", "POST"])
@@ -52,7 +63,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data, is_admin=True).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and handle_hashed_password_generate.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get("next")
             return (
